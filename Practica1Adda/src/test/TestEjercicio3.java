95aee01432d8f2e919ed9c59d55bd3fad05976ca
Migueel0 <miggaller@alum.us.es>
2022-10-23 22:12:41 +0200

    resolved wrong list type in exercise3 test

--- Practica1Adda/src/test/TestEjercicio3.java
+++ Practica1Adda/src/test/TestEjercicio3.java
@@ -16,8 +16,8 @@ public class TestEjercicio3 {
 		
 		Integer i =1;
 		while(i<=3) {
-			List<LineaFichero> fichero1 = ParseoFicheroEj3.listaDeFichero("Ficheros/PI1Ej3DatosEntrada" + i + "A.txt");
-			List<LineaFichero> fichero2 = ParseoFicheroEj3.listaDeFichero("Ficheros/PI1Ej3DatosEntrada" + i + "B.txt");
+			List<Punto2D> fichero1 = ParseoFicheroEj3.listaDeFichero("Ficheros/PI1Ej3DatosEntrada" + i + "A.txt");
+			List<Punto2D> fichero2 = ParseoFicheroEj3.listaDeFichero("Ficheros/PI1Ej3DatosEntrada" + i + "B.txt");
 		
 			System.out.println("Test"+ i+ ":\nLos siguientes "+Ejercicio3.ejercicio3Funcional(fichero1, fichero2).size()+" puntos:\n");
 			System.out.println("Test Iterativo" +(i)+":\n"+Ejercicio3.ejercicio3Iterativo(fichero1, fichero2)+"\n");
